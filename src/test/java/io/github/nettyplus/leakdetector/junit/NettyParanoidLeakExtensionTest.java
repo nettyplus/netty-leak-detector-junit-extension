5e831ab6e77e182eacf8763892a11ce034344274
sullis <github@seansullivan.com>
2024-02-17 10:00:12 -0800

    NettyParanoidLeakExtensionTest.java

--- src/test/java/io/github/nettyplus/leakdetector/junit/NettyParanoidLeakExtensionTest.java
+++ src/test/java/io/github/nettyplus/leakdetector/junit/NettyParanoidLeakExtensionTest.java
@@ -1,4 +1,12 @@
 package io.github.nettyplus.leakdetector.junit;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(NettyParanoidLeakExtension.class)
 public class NettyParanoidLeakExtensionTest {
+    @Test
+    void verifyLeakDetection() {
+        // TODO
+    }
 }
