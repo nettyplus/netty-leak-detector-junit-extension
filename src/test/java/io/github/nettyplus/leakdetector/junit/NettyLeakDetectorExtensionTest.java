0ef4df0bbcbcd5ce7c9ff9073aad816c70993cce
sullis <github@seansullivan.com>
2024-02-17 12:25:20 -0800

    enhance NettyLeakDetectorExtensionTest

--- src/test/java/io/github/nettyplus/leakdetector/junit/NettyLeakDetectorExtensionTest.java
+++ src/test/java/io/github/nettyplus/leakdetector/junit/NettyLeakDetectorExtensionTest.java
@@ -5,22 +5,21 @@ import io.netty.buffer.ByteBufAllocator;
 import io.netty.util.ResourceLeakDetector;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(NettyLeakDetectorExtension.class)
 public class NettyLeakDetectorExtensionTest {
+    private final NettyLeakDetectorExtension extension = new NettyLeakDetectorExtension();
+    static private final NettyLeakListener leakListener = NettyLeakDetectorExtension.getLeakListener();
+
     @Test
-    void verifyLeakDetection() {
-        for (int i = 0; i < 100; i++) {
+    void allocateByteBuf() {
+        leakListener.assertZeroLeaks();
+        for (int i = 0; i < 1000; i++) {
             ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(500);
             buf.ensureWritable(10);
             assertEquals(1, buf.refCnt());
-            buf.release();
-            assertEquals(0, buf.refCnt());
         }
     }
 
@@ -29,8 +28,6 @@ public class NettyLeakDetectorExtensionTest {
         assertTrue(ResourceLeakDetector.isEnabled());
         assertEquals(ResourceLeakDetector.Level.PARANOID, ResourceLeakDetector.getLevel());
         assertEquals("paranoid", System.getProperty( "io.netty.leakDetection.level"));
-        NettyLeakListener listener = NettyLeakDetectorExtension.getLeakListener();
-        assertNotNull(listener);
-        assertTrue(listener.getLeakCount() >= 0);
+        // TODO assertTrue(leakListener.getLeakCount() > 0);
     }
 }
