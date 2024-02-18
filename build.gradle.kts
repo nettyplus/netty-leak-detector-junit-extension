3fe5f188e147b882dcce5794fc7475e4e9993348
sullis <github@seansullivan.com>
2024-02-18 10:07:50 -0800

    Java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,10 +39,10 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
     toolchain {
-        languageVersion = JavaLanguageVersion.of(11)
+        languageVersion = JavaLanguageVersion.of(8)
     }
 }
 
