ec5c37f2eb2de26befb3f18af512c15d236f5dab
sullis <github@seansullivan.com>
2025-05-20 16:07:51 -0700

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ apply(from="gradle/shipkit.gradle.kts")
 
 group = "io.github.nettyplus"
 description = "netty-leak-detector-junit-extension"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
