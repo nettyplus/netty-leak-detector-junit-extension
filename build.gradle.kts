78447e91d61f3ee429fe0486d38c7d3b750fe53a
sullis <github@seansullivan.com>
2025-05-20 22:23:50 -0700

    version 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ apply(from="gradle/shipkit.gradle.kts")
 
 group = "io.github.nettyplus"
 description = "netty-leak-detector-junit-extension"
-version = "0.1.1"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
